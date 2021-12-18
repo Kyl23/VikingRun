5add3b3b2bc1a5caddfc257f6970ed59b24f9ecc
Kyl23 <cklkailiang@gmail.com>
2021-12-18 15:08:48 +0800

    feat: viking possible to change direction

--- Assets/Script/VikingController.cs
+++ Assets/Script/VikingController.cs
@@ -12,6 +12,7 @@ public class VikingController : MonoBehaviour
     public GameObject map;
     Rigidbody rb;
     Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,36 +20,34 @@ public class VikingController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
+    int yRotation = 0;
+    Vector3[,] vectorTable = { { Vector3.forward, Vector3.left, Vector3.back, Vector3.right},
+                               { Vector3.right, Vector3.forward, Vector3.left, Vector3.back},
+                               { Vector3.back, Vector3.right, Vector3.forward, Vector3.left},
+                               { Vector3.left, Vector3.back, Vector3.right, Vector3.forward},
+                             };
+    int[] rotationTable = { 0, 90, 180, 270 };
     // Update is called once per frame
     void Update()
     {
-        run = false;
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.localPosition += Time.deltaTime * movingSpeed * Vector3.forward;
-            run = true;
-        }
+        transform.localPosition += Time.deltaTime * movingSpeed * vectorTable[yRotation, 0];
         if (Input.GetKey(KeyCode.A))
         {
-            transform.localPosition += Time.deltaTime * movingSpeed * Vector3.left;
-            run = true;
+            transform.localPosition += Time.deltaTime * movingSpeed * vectorTable[yRotation, 1];
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.localPosition += Time.deltaTime * movingSpeed * Vector3.back;
-            run = true;
+            transform.localPosition += Time.deltaTime * movingSpeed * vectorTable[yRotation, 2];
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.localPosition += Time.deltaTime * movingSpeed * Vector3.right;
-            run = true;
+            transform.localPosition += Time.deltaTime * movingSpeed * vectorTable[yRotation, 3];
         }
         if (Input.GetKey(KeyCode.Space) && jump)
         {
             rb.AddForce(jumpForce * Time.deltaTime * Vector3.up);
             jump = false;
         }
-        animator.SetBool("Run", run);
         animator.SetBool("Jump", !jump);
         
         if (Input.GetMouseButtonDown(0))
@@ -63,9 +62,32 @@ public class VikingController : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            transform.rotation = new Quaternion(map.transform.localRotation.x, map.transform.localRotation.y - 90, map.transform.localRotation.z, map.transform.localRotation.w);
+            Quaternion trfm = map.transform.rotation;            
+            
+            setRotaionState(-1);
+            transform.rotation = Quaternion.Euler(Vector3.up * rotationTable[yRotation]);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Quaternion trfm = map.transform.rotation;
+
+            setRotaionState(1);
+            transform.rotation = Quaternion.Euler(Vector3.up * rotationTable[yRotation]);
+        }
+    }
+    void setRotaionState(int value)// value only be -1,1
+    {
+        if (value > 0)
+        {
+            if (yRotation == 3) yRotation = 0;
+            else yRotation += 1;
+        }
+        else
+        {
+            if (yRotation == 0) yRotation = 3;
+            else yRotation -= 1;
         }
     }
     void OnCollisionEnter(Collision collision)
